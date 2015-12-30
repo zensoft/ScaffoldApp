729db6c5129d75b8d3ddbd1edc9f906d49c8e6d1
Tomek <tomek.wojnowski@o2.pl>
2015-12-30 10:34:16 +0100

    Added logging types

--- settings/logging_settings.py
+++ settings/logging_settings.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from utils.log_type import LogType
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
 LOG_FILE = os.path.join(dir_path,"../out/app.log")
@@ -7,3 +8,4 @@ LOG_FORMAT = "%(asctime)s %(message)s"
 LOG_DATE = "%m/%d/%Y %H:%M:%S"
 LOG_LEVEL = logging.DEBUG
 LOG_FILE_MAX_LINE = 10000
+LOG_TYPE = LogType.SYS
--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -1,10 +1,15 @@
 from settings.logging_settings import *
+from utils.log_type import LogType
+import sys
 
 class CustomLogger:
     import logging
 
     def __init__(self):
-        self.logging.basicConfig(filename=LOG_FILE,level=LOG_LEVEL,format=LOG_FORMAT, datefmt=LOG_DATE)
+        if LOG_TYPE == LogType.SYS:
+            self.logging.basicConfig(level=LOG_LEVEL,format=LOG_FORMAT, datefmt=LOG_DATE, handlers=(logging.StreamHandler(sys.stdout),))
+        else:
+            self.logging.basicConfig(filename=LOG_FILE,level=LOG_LEVEL,format=LOG_FORMAT, datefmt=LOG_DATE)
         self._check_if_clear_log_file()
         pass
 
